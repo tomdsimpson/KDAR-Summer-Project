d466d71d420a479b926d7d5fbc47a6fa394b6406
tomdsimpson <tom@thesimpsonhouse.co.uk>
2026-06-02 11:19:16 +0100

    Background files and initial data

---
+++ test.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "This code was compliled with G++ version:" << __GNUC__ << "."
+	     << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << endl;
+	return 0;
+}
+
