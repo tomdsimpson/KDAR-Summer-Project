5d5994f045e655f28fdba9114df791ca0ae03cea
tomdsimpson <tom@thesimpsonhouse.co.uk>
2026-06-02 15:19:57 +0100

    test cpp files

--- test.cpp
+++ test.cpp
@@ -5,9 +5,10 @@
 
 int main() {
     // Open the ROOT file
-    TFile* file = TFile::Open("myfile.root", "READ");
+    TFile* file = TFile::Open("NuBeam_production_BooNE_50m_I174000A_0.dk2nu.root", "READ");
     
     if (!file || file->IsZombie()) {
         std::cerr << "Error: Cannot open file!" << std::endl;
         return 1;
     }
+}
